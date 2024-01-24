d7b92eb6df42152891b86efb0f028861ce9e4368
Fabs san <fabianagithub@gmail.com>
2024-01-24 18:43:38 +0000

    é

--- main.py
+++ main.py
@@ -29,6 +29,39 @@ class AnimaisDataBase:
 
     def ler_animal(self, id_animal):
 
-        for animal in self.animal:
+        for animal in self.animais:
             if animal.id_animal == id_animal:
                 return animal
+        print(f"Animal com ID {id_animal} não encontrado!")
+
+
+    def atualizar_animal(self, id_animal):
+        for animal in self.animais:
+            if animal.id_animal == id_animal:
+                nome_novo = input("Novo nome:")
+                idade_nova = int(input("Nova idade: "))
+                especie_nova = input("Nova especie: ")
+                raca_nova = input("Nova raça: ")
+
+                animal.nome = nome_novo
+                animal.idade = idade_nova
+                animal.especie = especie_nova
+                animal.raca = raca_nova
+                print(f"Animal {nome_novo} atualizado!")
+                return
+        print(f"Animal com ID {id_animal} não encontrado!")    
+
+
+    def remover_animal(self, id_animal):
+        for animal in self.animais:
+            if animal.id_animal == id_animal:
+                excluir_animal = Animal(id_animal, nome, idade, especie, raca)
+                self.animais.remove(excluir_animal)
+                print(f"Animal {nome} excluido com sucesso!")
+                return
+        print(f"Animal com ID {id_animal} não encontrado!")
+
+
+banco_de_dados = AnimaisDataBase()
+
+banco_de_dados.criar.animal()
